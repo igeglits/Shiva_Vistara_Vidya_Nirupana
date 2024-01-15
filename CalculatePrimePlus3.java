9fdcdd779644f99afd695e7f0c893b691fa5a60c
igor_eglit <igeglits@gmail.com>
2024-01-15 20:08:16 +0200

    поиск вещей

--- CalculatePrimePlus3.java
+++ CalculatePrimePlus3.java
@@ -12,12 +12,12 @@ public class CalculatePrimePlus3 {
         int num9 = 9;
 
         // Вызов метода processInput для получения суммы единичных чисел
-        int sumOfSingleDigits = processInput(num1, num2, num3, num4, num5, num6, num7, num8, num9);
+        int stack_of_numbers = processInput(num1, num2, num3, num4, num5, num6, num7, num8, num9);
 
         // Вызов метода addThreeAndReturn для добавления 3 к сумме единичных чисел
         System.out.print("Первое число = ");
-        System.out.println(sumOfSingleDigits);
-        int finalResult = addThreeAndReturn(sumOfSingleDigits);
+        System.out.println(stack_of_numbers);
+        int finalResult = addThreeAndReturn(stack_of_numbers);
 
         // Вывод результата
         System.out.println("Второе число = " + finalResult);
@@ -25,24 +25,24 @@ public class CalculatePrimePlus3 {
 
     // Метод для обработки входных чисел и получения суммы единичных чисел
     public static int processInput(int... numbers) {
-        int sum = 0;
+        int result_1 = 0;
 
         // Обработка каждого числа
         for (int num : numbers) {
             // Разбиение числа на цифры и нахождение суммы цифр
             char[] digits = String.valueOf(num).toCharArray();
             for (char digit : digits) {
-                sum += Character.getNumericValue(digit);
+                result_1 += Character.getNumericValue(digit);
             }
         }
 
         // Повторение процедуры, пока сумма не станет однозначным числом
-        while (sum > 81) {
-            sum = getDigitSum(sum);
+        while (result_1 > 81) {
+            result_1 = getDigitSum(result_1);
         }
 
         // Возвращение суммы единичных чисел
-        return sum;
+        return result_1;
     }
 
     // Метод для добавления 3 к числу и возвращения результата
@@ -50,7 +50,8 @@ public class CalculatePrimePlus3 {
         return num + 3;
     }
 
-    // Метод для получения суммы цифр числа
+    // Метод для получения суммы цифр числа. Отрезает каждое поледнюю цифру числа и скидывает его в сумму,
+    // единичное число сбрасывает без отрезания.
     private static int getDigitSum(int num) {
         int sum = 0;
         while (num != 0) {
@@ -58,5 +59,20 @@ public class CalculatePrimePlus3 {
             num /= 10;
         }
         return sum;
+/*
+        Пошаговое объяснение:
+        Инициализация переменной sum: Создается переменная sum и инициализируется значением 0.
+        В этой переменной будет храниться сумма цифр числа.
+        Цикл while (num != 0): Цикл будет выполняться, пока число num не станет равным 0.
+        sum += num % 10: Операция num % 10 возвращает последнюю цифру числа num (остаток от деления на 10),
+        и эта цифра добавляется к текущей сумме sum.
+                num /= 10: Операция num /= 10 удаляет последнюю цифру числа num,
+                таким образом, мы переходим к следующей цифре в числе.
+        Повторение цикла: Цикл повторяется снова, и шаги 3-4 выполняются для следующей цифры в числе.
+        Этот процесс продолжается до тех пор, пока все цифры числа не будут обработаны
+        (пока num не станет равным 0).
+        Возвращение суммы: После завершения цикла в переменной sum будет содержаться сумма всех цифр числа,
+        и это значение возвращается из метода.
+*/
     }
 }
